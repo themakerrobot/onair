02d0eabb83aafcc3e49196b1ba80a52b68db92da
leeyunjai82 <leeyunjai@circul.us>
2024-07-03 10:35:43 +0900

    update

---
+++ onair-app/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+requirements = [
+]
+
+test_requirements = [
+]
+
+setup(
+    name='onair-app',
+    version='0.0.0',
+    description="onair",
+    long_description=readme,
+    author="circulus",
+    author_email='leeyunjai@circul.us',
+    url='https://github.com/themakerrobot/onair-project',
+    packages=[],
+    package_dir={},
+    package_data={},
+    install_requires=requirements,
+    zip_safe=False,
+    keywords='onair',
+    classifiers=[
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.11',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements
+)
